2ce1d88c9f146f6fff882fda862a0a491c7d68d6
MahmoudNasser20 <152732588+Salieri20@users.noreply.github.com>
2025-08-11 04:08:46 +0300

    Delete src/org/example/CleanupStage.groovy

--- src/org/example/CleanupStage.groovy
+++
@@ -1,9 +0,0 @@
-package org.example
-
-class CleanupStage {
-    static void run(script) {
-        script.stage('Cleanup') {
-            script.sh "docker image prune -f || true"
-        }
-    }
-}
